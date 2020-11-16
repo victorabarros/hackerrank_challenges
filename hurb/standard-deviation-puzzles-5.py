77e1072a3b8fe5392b1a5d676030e0f77658dfad
Victor Barros <victorbarros1130@gmail.com>
2020-11-16 15:09:53 -0300

    standar deviation

---
+++ hurb/standard-deviation-puzzles-5.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/contests/desafio-de-algoritmos-hurb-1311/challenges/standard-deviation-puzzles-5
+mean = 45
+sum_scores = mean * 500
+
+new_mean = (sum_scores + 10 * 500) / 500
+
+print(new_mean)
